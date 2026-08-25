602142bdd59b053784b7f2f0610086f8bedc37a6
alejo180905 <acorream2@eafit.edu.co>
2026-08-25 16:18:31 -0500

    Tutorial 03: Laravel Dependency Injection, Service Providers, and Image Storage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ImageNotDIController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +17,13 @@ Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('pro
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
+Route::get('/cart/removeAll/', [CartController::class, 'removeAll'])->name('cart.removeAll');
+
+Route::get('/image', [ImageController::class, 'index'])->name('image.index');
+Route::post('/image/save', [ImageController::class, 'save'])->name('image.save');
+
+Route::get('/image-not-di', [ImageNotDIController::class, 'index'])->name('imagenotdi.index');
+Route::post('/image-not-di/save', [ImageNotDIController::class, 'save'])->name('imagenotdi.save');
